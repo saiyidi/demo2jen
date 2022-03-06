20cfa55de5d605e5f317e196663614c95b841371
saiyidi <issiway@gmail.com>
2022-03-06 12:45:42 +0100

    only app

---
+++ currency-exchange-sample/src/main/java/com/currencyexchange/microservices/currencyexchangeservice/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.samplecurrexchange.microservices.currencyexchangeservice;
+
+public class HelloWorld {
+
+    public String sayHello(String name) {
+
+        System.out.println("sayHello called with name : " + name);
+        return "Hello " + name;
+    }
+
+}
---
+++ currency-exchange-sample/src/test/java/com/currencyexchange/microservices/currencyexchangeservice/cucumber/CucumberSpringContextConfiguration.java
@@ -0,0 +1,19 @@
+package com.samplecurrexchange.microservices.currencyexchangeservice.cucumber;
+
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.samplecurrexchange.microservices.currencyexchangeservice.CurrencyExchangeServiceApplicationH2;
+
+import cucumber.api.java.Before;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = CurrencyExchangeServiceApplicationH2.class, loader = SpringBootContextLoader.class)
+public class CucumberSpringContextConfiguration {
+
+  @Before
+  public void setUp() {
+  }
+}
